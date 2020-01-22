f5a6edf6dc444ea9bcef27020033bb081ef5ed39
Toon Geens <toon.geens@xenit.eu>
2020-01-22 10:07:39 +0100

    Fix Alfresco content model namespace

--- ditto/src/main/java/eu/xenit/testing/ditto/api/data/ContentModel.java
+++ ditto/src/main/java/eu/xenit/testing/ditto/api/data/ContentModel.java
@@ -4,31 +4,31 @@ public interface ContentModel {
 
     interface Content {
 
-        String FOLDER = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}folder";
-        String CONTENT = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}content";
+        String FOLDER = "{http://www.alfresco.org/model/content/1.0}folder";
+        String CONTENT = "{http://www.alfresco.org/model/content/1.0}content";
 
-        String AUDITABLE = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}auditable";
+        String AUDITABLE = "{http://www.alfresco.org/model/content/1.0}auditable";
 
-        String NAME = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}name";
+        String NAME = "{http://www.alfresco.org/model/content/1.0}name";
 
-        String CREATOR = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}creator";
-        String CREATED = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}created";
-        String MODIFIER = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}modifier";
-        String MODIFIED = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/content/1.0}modified";
+        String CREATOR = "{http://www.alfresco.org/model/content/1.0}creator";
+        String CREATED = "{http://www.alfresco.org/model/content/1.0}created";
+        String MODIFIER = "{http://www.alfresco.org/model/content/1.0}modifier";
+        String MODIFIED = "{http://www.alfresco.org/model/content/1.0}modified";
 
     }
 
     interface System {
 
-        String STORE_ROOT = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/system/1.0}store_root";
+        String STORE_ROOT = "{http://www.alfresco.org/model/system/1.0}store_root";
 
-        String NODE_DBID = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/system/1.0}node-dbid";
+        String NODE_DBID = "{http://www.alfresco.org/model/system/1.0}node-dbid";
 
-        String STORE_PROTOCOL = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/system/1.0}store-protocol";
-        String STORE_IDENTIFIER = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/system/1.0}store-identifier";
-        String NODE_UUID = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/system/1.0}node-uuid";
+        String STORE_PROTOCOL = "{http://www.alfresco.org/model/system/1.0}store-protocol";
+        String STORE_IDENTIFIER = "{http://www.alfresco.org/model/system/1.0}store-identifier";
+        String NODE_UUID = "{http://www.alfresco.org/model/system/1.0}node-uuid";
 
-        String LOCALE = "{http://www.eu.xenit.testing.ditto.alfresco.org/model/system/1.0}locale";
+        String LOCALE = "{http://www.alfresco.org/model/system/1.0}locale";
 
     }
 
