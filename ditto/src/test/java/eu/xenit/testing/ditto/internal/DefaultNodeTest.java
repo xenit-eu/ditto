c44965b89d4fedd1a2cb7fbe5059dae5a20b49bb
Dries De Proost <dries.deproost@xenit.eu>
2020-02-14 14:45:58 +0100

    add test for a single parent

--- ditto/src/test/java/eu/xenit/testing/ditto/internal/DefaultNodeTest.java
+++ ditto/src/test/java/eu/xenit/testing/ditto/internal/DefaultNodeTest.java
@@ -4,12 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import eu.xenit.testing.ditto.api.AlfrescoDataSet;
 import eu.xenit.testing.ditto.api.data.ContentModel.Content;
 import eu.xenit.testing.ditto.api.model.MLText;
 import eu.xenit.testing.ditto.api.model.Node;
+import eu.xenit.testing.ditto.api.model.ParentChildNodeCollection;
 import eu.xenit.testing.ditto.internal.DefaultTransaction.TransactionContext;
 import java.time.Instant;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class DefaultNodeTest {
@@ -56,5 +60,44 @@ class DefaultNodeTest {
         assertThat(node.getProperties().get(Content.NAME)).hasValue(name);
 
     }
+
+    @Test
+    void parentAssocsSingle() {
+        final Node[] parents = new Node[1];
+        final String[] noderefs = new String[1];
+
+        AlfrescoDataSet dataSet = AlfrescoDataSet
+                .bootstrapAlfresco(Instant.now())
+                .skipToTransaction(123L)
+                .addTransaction(txn -> {
+                    parents[0] = txn.addNode(doc -> {
+                        doc.type(Content.FOLDER);
+                        doc.name("foo");
+                        doc.property("cm:description", "Folder description");
+                    });
+                })
+                .skipToTransaction(456L)
+                .addTransaction((txn -> {
+                    txn.addNode(parents[0], doc -> {
+                        doc.type(Content.CONTENT);
+                        doc.name("bar.txt");
+                        doc.content("bar");
+                        doc.property("cm:description", "Document description");
+                        noderefs[0] = doc.nodeRef().toString();
+                    });
+                }))
+                .build();
+        Optional<Node> node1 = dataSet.getNodeView().getNode(noderefs[0]);
+        assertThat(node1).isPresent().hasValueSatisfying(value -> {
+            assertThat(value).isInstanceOf(Node.class)
+                    .satisfies(node -> {
+                        ParentChildNodeCollection parentNodeCollection = node.getParentNodeCollection();
+                        assertThat(parentNodeCollection.getAssociations().count()).isEqualTo(1);
+                        assertThat(parentNodeCollection.getAssociations().collect(Collectors.toList())
+                                .get(0).getParent()).isEqualTo(parents[0]);
+                    });
+        });
+    }
+
 
 }
